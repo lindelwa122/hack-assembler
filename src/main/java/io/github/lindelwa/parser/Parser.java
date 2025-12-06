8f3da4d5ac7ca6653019c7c2252192da1b3756f1
Nqabenhle <lindelwa.codes@gmail.com>
2025-12-06 10:47:35 +0200

    Add a method to parse in the assembly code and perform the conversion

--- src/main/java/io/github/lindelwa/parser/Parser.java
+++ src/main/java/io/github/lindelwa/parser/Parser.java
@@ -1,22 +1,21 @@
 package io.github.lindelwa.parser;
 
+import io.github.lindelwa.code.Code;
 import io.github.lindelwa.exceptions.CommandException;
 import io.github.lindelwa.exceptions.SyntaxException;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 /**
  * Reads an assembly language command, parses it, and provides convenient
  * access to the command's components (fields & registers)
  */
 public class Parser {
-    private List<String> program = new ArrayList<>();
+    private final List<String> program = new ArrayList<>();
     private int programCounter = 0;
     private String currentCommand = "";
+    private final Map<String, Integer> symbolTable = new HashMap<>();
 
     public enum Command {
         A_COMMAND, C_COMMAND, L_COMMAND;
@@ -37,6 +36,132 @@ public class Parser {
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
         }
+
+        // Fill in the symbolTable with define symbols
+        this.symbolTable.put("SP", 0);
+        this.symbolTable.put("LCL", 1);
+        this.symbolTable.put("ARG", 2);
+        this.symbolTable.put("THIS", 3);
+        this.symbolTable.put("THAT", 4);
+        this.symbolTable.put("R0", 0);
+        this.symbolTable.put("R1", 1);
+        this.symbolTable.put("R2", 2);
+        this.symbolTable.put("R3", 3);
+        this.symbolTable.put("R4", 4);
+        this.symbolTable.put("R5", 5);
+        this.symbolTable.put("R6", 6);
+        this.symbolTable.put("R7", 7);
+        this.symbolTable.put("R8", 8);
+        this.symbolTable.put("R9", 9);
+        this.symbolTable.put("R10", 10);
+        this.symbolTable.put("R11", 11);
+        this.symbolTable.put("R12", 12);
+        this.symbolTable.put("R13", 13);
+        this.symbolTable.put("R14", 14);
+        this.symbolTable.put("R15", 15);
+        this.symbolTable.put("SCREEN", 16384);
+        this.symbolTable.put("KBD", 24576);
+    }
+
+    private void writeOutput(String filePath, String binaryCode) {
+        try (FileWriter fw = new FileWriter(filePath, true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+
+            bw.write(binaryCode + "\n");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void reset() {
+        this.currentCommand = "";
+        this.programCounter = 0;
+    }
+
+    public void parse(String filePath) throws SyntaxException, CommandException {
+        // FIRST PASS: Go through the entire program and build the symbol
+        // table without generating any code.
+        int romAddress = 0;
+        while (this.hasMoreCommands()) {
+            this.advance();
+
+            Command commandType = this.commandType();
+            if (commandType == Command.A_COMMAND
+                    || commandType == Command.C_COMMAND)
+            {
+                romAddress++;
+            }
+
+            else if (commandType == Command.L_COMMAND) {
+                String label = symbol();
+                this.symbolTable.put(label, romAddress+1);
+            }
+        }
+
+        this.reset();
+
+        // SECOND PASS: Now go again through the entire program, and parse
+        // each line. Each time a symbolic A-instruction is encountered,
+        // namely, @Xxx where Xxx is a symbol and not a number, look up
+        // Xxx in the symbol table. If the symbol is found in the table,
+        // then replace its numeric meaning and complete the command's
+        // translation. If the symbol is not found in the table, then it
+        // must represent a new variable. To handle it, add the pair
+        // (Xxx, n) to the symbol table, where n is the next available
+        // RAM address, and complete the command's translation. The
+        // allocated RAM addresses are consecutive numbers, starting
+        // at address 16
+        int ramAddress = 16;
+        while (this.hasMoreCommands()) {
+            this.advance();
+
+            Command commandType = this.commandType();
+            switch (commandType) {
+                case A_COMMAND -> {
+                    String symbol = this.symbol();
+                    if (!this.isStringNumeric(symbol)
+                            && !this.symbolTable.containsKey(symbol))
+                    {
+                        this.symbolTable.put(symbol, ramAddress);
+                        symbol = Integer.toString(ramAddress);
+                        ramAddress++;
+                    }
+
+                    else if (!this.isStringNumeric(symbol)) {
+                        symbol = Integer.toString(ramAddress);
+                    }
+
+                    // Convert symbol (numeric) to binary string and write to output
+                    int symbolAsInt = Integer.parseInt(symbol);
+                    if (symbolAsInt < 0) throw new SyntaxException(
+                            "the A-instruction must refer to address from 0"
+                    );
+
+                    String binaryString = Integer.toBinaryString(symbolAsInt);
+                    binaryString = "0".repeat(15 - binaryString.length()) + binaryString;
+                    this.writeOutput(filePath, "0" + binaryString);
+                }
+
+                case C_COMMAND -> {
+                    String compBin = Code.comp(this.comp());
+                    String destBin = Code.dest(this.dest());
+                    String jumpBin = Code.jump(this.jump());
+
+                    String binaryString = "111" + compBin + destBin + jumpBin;
+                    this.writeOutput(filePath, binaryString);
+                }
+            }
+        }
+    }
+
+    private boolean isStringNumeric(String s) {
+        if (s == null || s.isEmpty()) return false;
+
+        for (char c : s.toCharArray()) {
+            if (!Character.isDigit(c)) return false;
+        }
+
+        return true;
     }
 
     /**
@@ -87,6 +212,9 @@ public class Parser {
         {
             return Command.C_COMMAND;
         }
+
+        // Comment starts with double forward backslashes (//)
+        if (this.currentCommand.startsWith("//")) return null;
 
         throw new SyntaxException(
                 this.constructErrorMsg("instruction not recognized")
